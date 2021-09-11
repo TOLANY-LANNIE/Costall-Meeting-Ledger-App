b37b8c11aa2de06b774a63d0708b3ed6fa3e164c
TOLANY-LANNIE <tolanym_2@live.com>
2021-09-11 10:25:29 +0200

    added a auth fragments

---
+++ app/src/main/java/com/m/costallmeetingslogger/activities/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.m.costallmeetingslogger.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.m.costallmeetingslogger.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
