78affb52a071c9b4303e819cbae51aa6380870d4
TOLANY-LANNIE <tolanym_2@live.com>
2021-09-14 14:07:19 +0200

    changed made on the layouts

--- app/src/main/java/com/m/costallmeetingslogger/MainActivity.kt
+++ app/src/main/java/com/m/costallmeetingslogger/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.m.costallmeetingslogger
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.m.costallmeetingslogger.R
+import com.m.costallmeetingslogger.activities.AuthActivity
+import com.m.costallmeetingslogger.activities.SplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        finish()
+        startActivity(Intent(this,SplashScreen::class.java))
     }
 }
